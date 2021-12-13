14f2929ecc9d63cd94073fd9225bd0e3e38f8be6
HoangAnh <nguyehoanganh8976@gmail.com>
2021-12-13 23:32:56 +0700

    BT

---
+++ BT2/BT2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BT2
+{
+    internal class Bt2
+    {
+        static void Main(string[] args)
+        {
+            string ten;
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            ten = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            Console.WriteLine(" Hello " + ten);
+            Console.ReadLine();
+        }
+    }
+}
